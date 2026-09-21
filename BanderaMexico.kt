a226e99682ec6b77f49e5d50831f33fff0ffd1fa
Paola <diazaltamirapaola@gmail.com>
2026-09-21 08:25:38 -0600

    Practica 01: Bandera de Mexico con Row y Box

---
+++ BanderaMexico.kt
@@ -0,0 +1,60 @@
+package com.example.banderasjetpack
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BanderaMexico(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxSize()) {
+        // Franja verde[cite: 1]
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFF006341))
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFD4AF37))
+            )
+        }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFFCE1126))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaMexicoPreview() {
+    Surface {
+        BanderaMexico(modifier = Modifier.fillMaxSize())
+    }
+}
