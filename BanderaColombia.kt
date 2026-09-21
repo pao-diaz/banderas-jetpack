5691f78f3c4708b1f7d34854ac5a9656833f88ec
Paola <diazaltamirapaola@gmail.com>
2026-09-21 08:48:11 -0600

    Practica 06: Bandera de Colombia con proporciones en Column

---
+++ BanderaColombia.kt
@@ -0,0 +1,29 @@
+package com.example.banderasjetpack
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BanderaColombia(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Box(Modifier.weight(2f).fillMaxWidth().background(Color(0xFFFCD116))) // Amarillo (peso 2)
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFF003893))) // Azul (peso 1)
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFFCE1126))) // Rojo (peso 1)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaColombiaPreview() {
+    Surface {
+        BanderaColombia(modifier = Modifier.fillMaxSize())
+    }
+}
