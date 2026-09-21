f9abf759a51f3b3a1717686b9fdf87f6b45c6947
Paola <diazaltamirapaola@gmail.com>
2026-09-21 08:37:50 -0600

    Practica 04: Bandera de Alemania con Column

---
+++ BanderaAlemania.kt
@@ -0,0 +1,29 @@
+package com.example.banderasjetpack
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BanderaAlemania(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color.Black))       // Negro
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFFDD0000))) // Rojo[cite: 1]
+        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFFFFCE00))) // Amarillo[cite: 1]
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BanderaAlemaniaPreview() {
+    Surface {
+        BanderaAlemania(modifier = Modifier.fillMaxSize())
+    }
+}
