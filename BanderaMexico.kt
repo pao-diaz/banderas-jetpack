35d2a7b351af1b1b72566705bd9b7ee2c9d1bf9e
Paola <diazaltamirapaola@gmail.com>
2026-09-22 09:34:40 -0600

    agg cosas

--- BanderaMexico.kt
+++ BanderaMexico.kt
@@ -18,6 +18,22 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+class MainActivity: ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaMexico(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
 @Composable
 fun BanderaMexico(modifier: Modifier = Modifier) {
     Row(modifier = modifier.fillMaxSize()) {
