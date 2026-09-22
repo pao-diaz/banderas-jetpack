7c58e1e074e9f4ee83b534cc53a7387aee2d0adf
Paola <diazaltamirapaola@gmail.com>
2026-09-22 10:38:54 -0600

    agg cosas

--- BanderaEspana.kt
+++ BanderaEspana.kt
@@ -11,6 +11,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
+class MainActivity: ComponentActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme() {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaEspana(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
 @Composable
 fun BanderaEspana(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
